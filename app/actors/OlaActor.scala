28b12241d6917e3928379d648ca3fc30e8820636
Hans Speijer <hans.speijer@mindsteps.eu>
2016-03-24 18:20:41 +0100

    light classes - fix a

--- app/actors/OlaActor.scala
+++ app/actors/OlaActor.scala
@@ -41,75 +41,76 @@ class OlaActor extends Actor with ActorLogging {
       if (lightsMessage.fire) {
         lights(2).setARGB(255, 255, 0, 0)
         //client.sendDmx(0, Array[Short](127,127,0,0).padTo(512, 0.toShort))
-        // }
-        if (lightsMessage.water) {
-          lights(3).setARGB(255, 255, 0, 0)
-          //client.sendDmx(0, Array[Short](127,0,0,127).padTo(512, 0.toShort))
-        }
-        if (lightsMessage.aether) {
-          //client.sendDmx(0, Array[Short](127,127,127,127).padTo(512, 0.toShort))
-          lights(4).setARGB(255, 255, 255, 255)
-        }
-        sendDmx()
       }
-    }
-
-      def clearAll() = {
-        for (light <- lights) {
-          light.clear()
-        }
+      if (lightsMessage.water) {
+        lights(3).setARGB(255, 0, 0, 255)
+        //client.sendDmx(0, Array[Short](127,0,0,127).padTo(512, 0.toShort))
       }
-
-      def sendDmx(): Unit = {
-        var frame = Array[Short]().padTo(512, 0.toShort)
-        for (light <- lights) {
-          light.addToFrame(frame)
-        }
-        client.sendDmx(UNIVERSE_ID, frame)
+      if (lightsMessage.aether) {
+        //client.sendDmx(0, Array[Short](127,127,127,127).padTo(512, 0.toShort))
+        lights(4).setARGB(255, 255, 255, 255)
       }
-
-  }
-  
-  class ARGB(var a: Short = 0,
-             var r: Short = 0,
-             var g: Short = 0,
-             var b: Short = 0) {
-
-    def this() = {
-      this(0, 0, 0, 0)
-    }
-
-    def toArray() = {
-      Array[Short](a,r,g,b)
+      sendDmx()
     }
   }
 
-  class OlaLight(id: Int, argb : ARGB) {
-
-    def this(id : Int) = {
-      this(id, new ARGB(0, 0, 0, 0))
-    }
-
-    def clear() = {
-      setARGB(0, 0, 0, 0)
-    }
-
-    def setARGB(a: Short, r: Short, g: Short, b: Short) = {
-      argb.a = a
-      argb.r = r
-      argb.g = g
-      argb.b = b
-    }
-
-    def addToFrame(frameData: Array[Short]) = {
-      argb.toArray().copyToArray(frameData, id * 4)
+  def clearAll() = {
+    for (light <- lights) {
+      light.clear()
     }
   }
-  
-  class FadingLight(id: Short, argb : ARGB) extends OlaLight(id,argb) {
-    var currentStep = 0
-    var steps = 0
-    var startARGB = new ARGB()
-    var endARGB = new ARGB()
+
+  def sendDmx(): Unit = {
+    var frame = Array[Short]().padTo(512, 0.toShort)
+    for (light <- lights) {
+      light.addToFrame(frame)
+    }
+    client.sendDmx(UNIVERSE_ID, frame)
   }
+
+}
+
+class ARGB(var a: Short = 0,
+           var r: Short = 0,
+           var g: Short = 0,
+           var b: Short = 0) {
+
+  def this() = {
+    this(0, 0, 0, 0)
+  }
+
+  def toArray() = {
+    Array[Short](a, r, g, b)
+  }
+}
+
+class OlaLight(id: Int, argb: ARGB) {
+
+  def this(id: Int) = {
+    this(id, new ARGB(0, 0, 0, 0))
+  }
+
+  def clear() = {
+    setARGB(0, 0, 0, 0)
+  }
+
+  def setARGB(a: Short, r: Short, g: Short, b: Short) = {
+    argb.a = a
+    argb.r = r
+    argb.g = g
+    argb.b = b
+  }
+
+  def addToFrame(frameData: Array[Short]) = {
+    argb.toArray().copyToArray(frameData, id * 4)
+  }
+}
+
+class FadingLight(id: Short, argb: ARGB) extends OlaLight(id, argb) {
+  var currentStep = 0
+  var steps = 0
+  var startARGB = new ARGB()
+  var endARGB = new ARGB()
+}
+
 }
